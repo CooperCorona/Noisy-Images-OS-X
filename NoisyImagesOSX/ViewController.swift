24eaff4fb1b4ee6f32938beed39ffeb7aa33c09d
Cooper Knaak <cooperknaak@gmail.com>
2016-03-26 22:32:44 -0500

    Initial Commit

---
+++ NoisyImagesOSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NoisyImagesOSX
+//
+//  Created by Cooper Knaak on 3/26/16.
+//  Copyright © 2016 Cooper Knaak. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
